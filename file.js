1a23d163a388b378b28333135b48cd2a3d7e7eff
Lapulay <pasha.terminator007@mail.ru>
2022-06-23 11:09:28 +0300

    .

--- file.js
+++ file.js
@@ -1,17 +1,18 @@
 function DeliteRowFunction(r){
-  var i = r.parentNode.parentNode.rowIndex;
+  let i = r.parentNode.parentNode.rowIndex;
   document.getElementById("Table").deleteRow(i);
 }
 
 
 function myFunction() {
-  var input, filter, table, tr, td, i, txtValue;
+  let input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("Table");
   tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+  for (i = 0; i <= tr.length; i++) {
+    
+    td = tr[i];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -19,9 +20,10 @@ function myFunction() {
       } else {
         tr[i].style.display = "none";
       }
-    }       
-  }
+    } 
+  }      
 }
+
 
 
 // alert('Я javaScript'); 
